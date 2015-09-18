ac77d297565c17325b7caa340a48b09d93bc09e5
Brian Link <blink@pbs.org>
2015-09-18 16:44:22 -0400

    fixing routes, bumping patch version.

--- app/routes/api/release.js
+++ app/routes/api/release.js
@@ -10,6 +10,7 @@ var router = require('express').Router(),
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 router.post('/:slug', function(req, res)
--- app/routes/api/releases.js
+++ app/routes/api/releases.js
@@ -8,6 +8,7 @@ var router = require('express').Router(),
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin": "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 router.get('/:slug', function(req, res)
