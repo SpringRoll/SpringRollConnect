6dc68813cd30cdbbad5bc267089840412a46578a
Brian Link <blink@pbs.org>
2015-09-21 09:33:03 -0400

    fixing CORS header in releases.js; bumping patch version

--- app/routes/api/releases.js
+++ app/routes/api/releases.js
@@ -6,7 +6,7 @@ var router = require('express').Router(),
 	response = require('../../helpers/response');
 
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin": "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
