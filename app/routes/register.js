fa7787ef1a645d437a131075a06e995152cf641d
Matt Karl <matt@cloudkid.com>
2015-11-16 15:43:54 -0500

    Added validation to registration form, updated library, added query string to embed

--- app/routes/register.js
+++ app/routes/register.js
@@ -11,6 +11,21 @@ router.get('/', function(req, res)
 
 router.post('/', function(req, res, next)
 {
+    // Do some basic validation
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'Username must only be alpha characters').isAlpha();
+    req.checkBody('email', 'Email must be a valid email address').isEmail();
+
+    var errors = req.validationErrors();
+
+    if (errors)
+    {
+        return res.render('register',
+        {
+            errors: errors
+        });
+    }
+
 	// Make sure that someone can override
 	// the user default privilege, would be 
 	// ugly to expose edit of this to anonymous
