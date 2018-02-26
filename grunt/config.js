fc9e2ab657cb676165754ba4e06e84fc30eba448
Chip Bell <cbell@pbs.org>
2018-02-26 11:13:14 -0500

    Adding in font copy tasks from old build

--- grunt/config.js
+++ grunt/config.js
@@ -1,5 +1,18 @@
 // initial grunt config. register-module, appends onto this with tasks
 module.exports = {
   concat: {},
-  less: {}
+  less: {},
+  copy: {
+    libraries: {
+      files: [
+        {
+          expand: true,
+          flatten: true,
+          src: ['components/bootstrap/dist/fonts/**'],
+          dest: 'app/public/fonts',
+          filter: 'isFile'
+        }
+      ]
+    }
+  }
 };
