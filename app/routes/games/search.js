14531d880bb28f8430ddee33d820476d9a155fee
Andrew Smith <andrew.smith@redspace.com>
2018-04-23 17:38:28 -0300

    added 'if' in game search to prevent looping around null

--- app/routes/games/search.js
+++ app/routes/games/search.js
@@ -2,44 +2,41 @@ var router = require('express').Router();
 var Game = require('../../models/game');
 var GameArchive = require('../../models/game-archive');
 
-router.post('/', function(req, res)
-{
-	var searches;
+router.post('/', function(req, res) {
+  var searches;
+  if (req.body.slug) {
+    searches = [
+      Game.getBySlug(req.body.slug).select('slug'),
+      GameArchive.getBySlug(req.body.slug).select('slug')
+    ];
+  } else if (req.body.bundleId) {
+    searches = [
+      Game.getByBundleId(req.body.bundleId).select('slug'),
 
-	if (req.body.slug)
-	{
-		searches = [
-			Game.getBySlug(req.body.slug).select('slug'),
-			GameArchive.getBySlug(req.body.slug).select('slug')
-		];
-	}
-	else if (req.body.bundleId)
-	{
-		searches = [
-			Game.getByBundleId(req.body.bundleId).select('slug'),
-			GameArchive.getByBundleId(req.body.bundleId).select('slug')
-		];
-	}
-	else if (req.body.search)
-	{
-		searches = [
-			Game.getBySearch(req.body.search, 10),
-			GameArchive.getBySearch(req.body.search, 10)
-		];
-	}
+      GameArchive.getByBundleId(req.body.bundleId).select('slug')
+    ];
+  } else if (req.body.search) {
+    searches = [
+      Game.getBySearch(req.body.search, 10),
+      GameArchive.getBySearch(req.body.search, 10)
+    ];
+  }
 
-	Promise.all(searches).then(function(data) {
-		data = JSON.parse(JSON.stringify(data));
+  Promise.all(searches).then(function(data) {
+    data = JSON.parse(JSON.stringify(data));
+    if (null !== data[0]) {
+      for (var i = 0; i < data[0].length; i++) {
+        data[0][i].url = '/games/game/';
+      }
+    }
+    if (null !== data[0]) {
+      for (var n = 0; n < data[1].length; n++) {
+        data[1][n].url = '/archive/game/';
+      }
+    }
 
-		for (var i = 0; i < data[0].length; i++) {
-			data[0][i].url = '/games/game/';
-		}
-		for (var n = 0; n < data[1].length; n++) {
-			data[1][n].url = '/archive/game/';
-		}
-
-		res.send([].concat.apply([], data));
-	});
+    res.send([].concat.apply([], data));
+  });
 });
 
 module.exports = router;
