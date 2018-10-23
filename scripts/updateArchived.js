6627e60a9f37e2e8f92c5212d52364d9f6e35efe
Dylan A. Orshefsky <daorshefsky@pbs.org>
2018-10-23 15:26:08 -0400

    fix gamearchive collection name, fix delete release bug

--- scripts/updateArchived.js
+++ scripts/updateArchived.js
@@ -17,9 +17,10 @@ mongoose.connect(process.env.MONGO_DATABASE, function()
     {multi: true}
   );
   console.log('Active games have been updated.');
-  mongoose.connection.db.collection('game-archive').find({}).forEach(game => {
+  mongoose.connection.db.collection('gamearchives').find({}).forEach(game => {
     game.isArchived = true;
     mongoose.connection.db.collection('games').insert(game);
   });
   console.log('Archived games transferred.');
+  return;
 });
