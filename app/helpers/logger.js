79c119a213de561dd88d89eb79af074c4b8babb8
Chip Bell <cbell@pbs.org>
2019-01-03 10:12:58 -0500

    Logging configuration tweaks
    - Renamed the logger name to be more appropriate
    - Updated logging configuration to also log to stdout in non-production
      environments

--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -1,12 +1,23 @@
+// Declare an "log stream" for bunyan to write to a file
+var streams = [
+  {
+    level: 'info',
+    path: process.env.OUTPUT_LOG
+  }
+];
+
+// If we're not in production also log to standard out
+if (process.env.NODE_ENV !== 'production') {
+  streams.push({
+    level: 'info',
+    stream: process.stdout
+  });
+}
+
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({
-	name: 'springroll-deployment',
-  streams: [
-    {
-      level: 'info',
-      path: process.env.OUTPUT_LOG
-    }
-  ]
+  name: 'springroll-connect',
+  streams: streams
 });
 
 module.exports = log;
