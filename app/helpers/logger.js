91c64f51c7312b704c6e915962d895c992aa68cc
Chip Bell <cbell@pbs.org>
2019-01-03 09:41:43 -0500

    Better formatted

--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -1,11 +1,12 @@
 var bunyan = require('bunyan');
 var log = bunyan.createLogger({
 	name: 'springroll-deployment',
-//    src: true,
-    streams: [{
-		level: 'info',
-		path: process.env.OUTPUT_LOG
-	}]
+  streams: [
+    {
+      level: 'info',
+      path: process.env.OUTPUT_LOG
+    }
+  ]
 });
 
 module.exports = log;
