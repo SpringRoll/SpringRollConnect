4bdce4d11ba54cbbfbac5b4b4412bb1e76f892fd
Chip Bell <cbell@pbs.org>
2019-01-10 09:15:10 -0500

    Adding some doc blocks

--- test/helpers/selenium.js
+++ test/helpers/selenium.js
@@ -2,16 +2,32 @@ const webdriver = require('selenium-webdriver');
 const chromedriver = require('chromedriver');
 
 const selenium = {
+  /**
+   * Initializes the underlying selenium browser implementation so that it can be used in a test
+   * @return WebDriver The web driver that was instantiated
+   * @see selenium.browser
+   */
   init: () => {
-    // bind the little helper function for improve DOM element queries
-    selenium.By = webdriver.By;
-
     selenium.browser = new webdriver.Builder()
       .forBrowser('chrome')
       .build();
 
     return selenium.browser;
-  }
+  },
+
+  /**
+   * The selenium web driver instance that is created by the init method
+   * @type WebDriver
+   * @see {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_WebDriver.html}
+   */
+  browser: null,
+
+  /**
+   * A reference to the selenium By object, for querying DOM elements on the page
+   * @type By
+   * @see {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_By.html}
+   */
+  By: webdriver.By
 }
 
 module.exports = selenium;
--- test/helpers/server.js
+++ test/helpers/server.js
@@ -1,6 +1,10 @@
 const spawn = require('child_process').spawn;
 
 const server = {
+  /**
+   * Initializes a running instance of the SpringRollConnect server so that we can make requests to it
+   * @return Promise A promise that resolves when the server is setup
+   */
   init: () => {
     return new Promise(resolve => {
       server.process = spawn('node', ['server.js']);
@@ -10,7 +14,14 @@ const server = {
         resolve();
       }, 1000);
     });
-  }
+  },
+
+  /**
+   * A reference to the child process that was spawned.
+   * @type ChildProcess
+   * @see {@link https://nodejs.org/api/child_process.html#child_process_class_childprocess}
+   */
+  process: null,
 };
 
 module.exports = server;
