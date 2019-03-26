b8311da28267060eb3258b34e9c125ced31c2fda
Andrew Smith <andrew.smith@redspace.com>
2019-03-26 15:56:06 -0300

    added await to game save

--- app/routes/api/release.js
+++ app/routes/api/release.js
@@ -1,197 +1,191 @@
 var router = require('express').Router(),
-	async = require('async'),
-	Game = require('../../models/game'),
-	Group = require('../../models/group'),
-	Release = require('../../models/release'),
-	response = require('../../helpers/response'),
-    log = require('../../helpers/logger');
+  async = require('async'),
+  Game = require('../../models/game'),
+  Group = require('../../models/group'),
+  Release = require('../../models/release'),
+  response = require('../../helpers/response'),
+  log = require('../../helpers/logger');
 
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
 });
 
-router.post('/:slug', function(req, res)
-{
-	req.checkBody('status', 'Status must be one of: "dev", "qa", "stage", "prod"').isStatus();
-	req.checkBody('commitId', 'Commit ID must be a valid Git commit has').isCommit();
-	req.checkBody('token', 'Token is required').isToken();
-	req.checkBody('branch', 'Branch is required and must be a string').isBranch();
-	
-	if (req.body.verison)
-		req.checkBody('version', 'Not a properly formatted Semantic Version').isSemver();
+router.post('/:slug', function(req, res) {
+  req
+    .checkBody('status', 'Status must be one of: "dev", "qa", "stage", "prod"')
+    .isStatus();
+  req
+    .checkBody('commitId', 'Commit ID must be a valid Git commit has')
+    .isCommit();
+  req.checkBody('token', 'Token is required').isToken();
+  req.checkBody('branch', 'Branch is required and must be a string').isBranch();
 
-	var errors = req.validationErrors();
+  if (req.body.verison)
+    req
+      .checkBody('version', 'Not a properly formatted Semantic Version')
+      .isSemver();
 
-	if (errors)
-	{
-		log.error("Validation error adding release from token " + req.body.token);
-		log.error(errors);
+  var errors = req.validationErrors();
 
-		if (req.body.redirect)
-		{
-			log.warn('Redirecting to ' + req.body.redirect);
-			res.redirect(req.body.redirect);
-		}
-		else
-		{
-			res.send({
-				success:false,
-				data: errors
-			});
-		}
-		return;
-	}
+  if (errors) {
+    log.error('Validation error adding release from token ' + req.body.token);
+    log.error(errors);
 
-	async.waterfall([
-		function(done)
-		{
-			Game.getBySlugOrBundleId(req.params.slug, done).select('-thumbnail');
-		},
-		function(game, done)
-		{
-			game.hasPermission(req.body.token, done);
-		},
-		function(game, done)
-		{
-			// Better handling of a unique commitId
-			Release.getByCommitId(req.body.commitId, function(err, release)
-			{
-				if (!!req.body.warnUniqueCommit && !!release)
-				{
-					done('The Commit ID is already taken');
-				}
-				else
-				{
-					done(err, game, release);
-				}
-			});
-		},
-		function(game, release, done)
-		{
-			// If we already have a release
-			// lets just modify the updated timestamp
-			// and leave everything else the same
-			if (release)
-			{
-				release.updated = Date.now();
-				release.save(function(err)
-				{
-					done(err, game);
-				});
-				return;
-			}
-			var values = Object.assign({}, values, req.body);
-			values.game = game._id;
-			delete values.token;
-			values.created = values.updated = Date.now();
+    if (req.body.redirect) {
+      log.warn('Redirecting to ' + req.body.redirect);
+      res.redirect(req.body.redirect);
+    } else {
+      res.send({
+        success: false,
+        data: errors
+      });
+    }
+    return;
+  }
 
-			// If the capabilities aren't set, inherit the
-			// default game capabilities
-			if (!values.capabilities)
-			{
-				values.capabilities = game.capabilities.toObject();
-			}
-			// Or else update the game defaults
-			else
-			{
-				Object.assign(
-					game.capabilities, 
-					Object.assign({}, values.capabilities)
-				);
-				game.save();
-			}
-			var newRelease = new Release(values);
-			newRelease.save(function(err, release)
-			{
-				if (err) return done(err, game);
+  async.waterfall(
+    [
+      function(done) {
+        Game.getBySlugOrBundleId(req.params.slug, done).select('-thumbnail');
+      },
+      function(game, done) {
+        game.hasPermission(req.body.token, done);
+      },
+      function(game, done) {
+        // Better handling of a unique commitId
+        Release.getByCommitId(req.body.commitId, function(err, release) {
+          if (!!req.body.warnUniqueCommit && !!release) {
+            done('The Commit ID is already taken');
+          } else {
+            done(err, game, release);
+          }
+        });
+      },
+      async function(game, release, done) {
+        // If we already have a release
+        // lets just modify the updated timestamp
+        // and leave everything else the same
+        if (release) {
+          release.updated = Date.now();
+          release.save(function(err) {
+            done(err, game);
+          });
+          return;
+        }
+        var values = Object.assign({}, values, req.body);
+        values.game = game._id;
+        delete values.token;
+        values.created = values.updated = Date.now();
 
-				game.releases.push(release._id);
-				game.updated = Date.now();
-				game.save(function(err, result)
-				{
-					done(err, game);
-				});
-			});
-		},
-		function(game, done)
-		{
-			Release.getByIdsAndStatus(game.releases, "dev", function(err, releases)
-			{
-				var maxDevReleases = CONFIGURATION.maxDevReleases;
-				if (releases.length > maxDevReleases)
-				{
-					let toSave = [];
-					while (toSave.length < maxDevReleases){
-						toSave.push(releases.pop());
-					}
-					releases.forEach(function(release)
-					{
-						Release.removeById(release._id, function(){});
-					});
-				}
-				done(null, game);
-			});
-		}
-	], 
-	function(err, result)
-	{
-		if (err)
-		{			
-			log.error('Unable to add the release for token ' + req.body.token);
-			log.error(err);
+        // If the capabilities aren't set, inherit the
+        // default game capabilities
+        if (!values.capabilities) {
+          values.capabilities = game.capabilities.toObject();
+        } else {
+          // Or else update the game defaults
+          Object.assign(
+            game.capabilities,
+            Object.assign({}, values.capabilities)
+          );
+          await game.save();
+        }
+        var newRelease = new Release(values);
+        newRelease.save(function(err, release) {
+          if (err) {
+            return done(err, game);
+          }
 
-			if (req.body.redirect)
-			{
-				res.redirect(req.body.redirect);
-			}
-			else
-			{
-				res.status(500).send({
-					success:false,
-					data: 'Unable to add the release'
-				});
-			}
-			return;
-		}
+          game.releases.push(release._id);
+          game.updated = Date.now();
+          game.save(function(err) {
+            done(err, game);
+          });
+        });
+      },
+      function(game, done) {
+        Release.getByIdsAndStatus(game.releases, 'dev', function(
+          err,
+          releases
+        ) {
+          var maxDevReleases = CONFIGURATION.maxDevReleases;
+          if (releases.length > maxDevReleases) {
+            let toSave = [];
+            while (toSave.length < maxDevReleases) {
+              toSave.push(releases.pop());
+            }
+            releases.forEach(function(release) {
+              Release.removeById(release._id, function() {});
+            });
+          }
+          done(null, game);
+        });
+      }
+    ],
+    function(err, result) {
+      if (err) {
+        log.error('Unable to add the release for token ' + req.body.token);
+        log.error(err);
 
-		if (req.body.redirect)
-		{
-			res.redirect(req.body.redirect);
-		}
-		else
-		{
-			res.send({
-				success: true, 
-				data: result
-			});
-		}
-	});
+        if (req.body.redirect) {
+          res.redirect(req.body.redirect);
+        } else {
+          res.status(500).send({
+            success: false,
+            data: 'Unable to add the release'
+          });
+        }
+        return;
+      }
+
+      if (req.body.redirect) {
+        res.redirect(req.body.redirect);
+      } else {
+        res.send({
+          success: true,
+          data: result
+        });
+      }
+    }
+  );
 });
 
-router.get('/:slugOrBundleId', function(req, res)
-{
-	req.checkQuery('token').optional().isToken();
-	req.checkQuery('status').optional().isStatus();
-	req.checkQuery('commitId').optional().isCommit();
-	req.checkQuery('version').optional().isSemver();
-	if (req.validationErrors())
-	{
-		return response.call(res, "Invalid arguments");
-	}
-	Release.getByGame(
-		req.params.slugOrBundleId, 
-		{
-			version: req.query.version,
-			commitId: req.query.commitId,
-			archive: req.query.archive,
-			status: req.query.status || 'prod',
-			token: req.query.token,
-			debug: req.query.debug
-		}, 
-		response.bind(res)
-	);
+router.get('/:slugOrBundleId', function(req, res) {
+  req
+    .checkQuery('token')
+    .optional()
+    .isToken();
+  req
+    .checkQuery('status')
+    .optional()
+    .isStatus();
+  req
+    .checkQuery('commitId')
+    .optional()
+    .isCommit();
+  req
+    .checkQuery('version')
+    .optional()
+    .isSemver();
+  if (req.validationErrors()) {
+    return response.call(res, 'Invalid arguments');
+  }
+  Release.getByGame(
+    req.params.slugOrBundleId,
+    {
+      version: req.query.version,
+      commitId: req.query.commitId,
+      archive: req.query.archive,
+      status: req.query.status || 'prod',
+      token: req.query.token,
+      debug: req.query.debug
+    },
+    response.bind(res)
+  );
 });
 
 module.exports = router;
