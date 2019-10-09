5e590049be20b51d60f7c344b8bf5ecdbad72a33
Sean Ryan <902seanryan@gmail.com>
2019-10-09 13:50:25 -0300

    Add test to check for new api functionality.

--- test/api/games.js
+++ test/api/games.js
@@ -1,10 +1,15 @@
 import { expect } from 'chai';
-import { createUserGroupGameRelease, API_GAMES_URL } from '../helpers';
+import {
+  createUserGroupGameRelease,
+  API_GAMES_URL,
+  makeGame,
+  makeRelease
+} from '../helpers';
 import fetch from 'node-fetch';
 
 describe('api/games', () => {
   describe('GET', () => {
-    it('should receive a list of all games with a production release, without requiring a token', async () => {
+    it('should receive a list of all games, with a production release if they have one, without requiring a token', async () => {
       await createUserGroupGameRelease();
       const response = await fetch(API_GAMES_URL).then(
         async r => await r.json()
@@ -14,6 +19,27 @@ describe('api/games', () => {
       expect(response.data.length).to.equal(1);
       expect(response.data[0].releases.length).to.equal(1);
     });
+
+    it('should recieve a list of only games with a prod release, without requiring a token', async () => {
+      const groupUserGameReleaseProd = await createUserGroupGameRelease();
+
+      const devGame = await makeGame({
+        name: 'Dev Game',
+        slug: 'dev-game',
+        bundleId: 'com.domain.game.SomeDevGame',
+        groups: [{ permission: 0, group: groupUserGameReleaseProd.group._id }]
+      });
+      const devRelease = await makeRelease(devGame, 'dev');
+
+      const response = await fetch(API_GAMES_URL + '?status=prod').then(
+        async r => await r.json()
+      );
+
+      expect(response.success).to.be.true;
+      expect(response.data.length).to.equal(1);
+      expect(response.data[0].releases.length).to.equal(1);
+    });
+
     it('should receive a list of all games, regardless of release level, if provided a token', async function() {
       const { group } = await createUserGroupGameRelease({
         privilege: 2,
