8f13739a1275f34f1619de0643d384a045f33524
Sean Ryan <902seanryan@gmail.com>
2019-10-09 14:01:18 -0300

    update api/games test to be more comprehensive

--- test/api/games.js
+++ test/api/games.js
@@ -31,13 +31,22 @@ describe('api/games', () => {
       });
       const devRelease = await makeRelease(devGame, 'dev');
 
-      const response = await fetch(API_GAMES_URL + '?status=prod').then(
+      const prodResponse = await fetch(API_GAMES_URL + '?status=prod').then(
         async r => await r.json()
       );
 
-      expect(response.success).to.be.true;
-      expect(response.data.length).to.equal(1);
-      expect(response.data[0].releases.length).to.equal(1);
+      expect(prodResponse.success).to.be.true;
+      expect(prodResponse.data.length).to.equal(1);
+      expect(prodResponse.data[0].releases.length).to.equal(1);
+
+      const allResponse = await fetch(API_GAMES_URL).then(
+        async r => await r.json()
+      );
+
+      expect(allResponse.success).to.be.true;
+      expect(allResponse.data.length).to.equal(2);
+      expect(allResponse.data[0].releases.length).to.equal(1);
+      expect(allResponse.data[1].releases.length).to.equal(0);
     });
 
     it('should receive a list of all games, regardless of release level, if provided a token', async function() {
