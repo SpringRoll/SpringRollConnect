4474e0054881058e806d5d62faf0fb4f1fdbc07c
Sean Ryan <902seanryan@gmail.com>
2019-10-24 10:05:51 -0300

    remove headless and update time out time for tests

--- test/helpers/selenium.ts
+++ test/helpers/selenium.ts
@@ -7,7 +7,7 @@ export const browser = new Builder()
   .setChromeOptions(
     new Options().addArguments(
       '--no-sandbox',
-      //'--headless',
+      '--headless',
       //Some Elements are hidden at smaller browser sizes
       'window-size=1240,720'
     )
