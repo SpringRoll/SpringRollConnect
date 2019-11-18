32677119b69a9607396ef70376715092cbbcbfab
Chip Bell <cbell@pbs.org>
2019-11-18 11:40:26 -0500

    Fixing an issue with the commitID widget on the embed page

--- src/widgets/version-to-commit.js
+++ src/widgets/version-to-commit.js
@@ -6,14 +6,16 @@ window.addEventListener('load', () => {
   const version = versionAndCommitButton.dataset.ver;
   let commitID = versionAndCommitButton.dataset.commit;
 
-  if (version && commitID) {
-    commitID = commitID.slice(0, 7);
-    versionAndCommitButton.addEventListener('click', function() {
-      if (versionAndCommitButton.textContent === version) {
-        versionAndCommitButton.textContent = commitID;
-      } else {
-        versionAndCommitButton.textContent = version;
-      }
-    });
+  if (!version || !commitID || !versionAndCommitButton) {
+    return;
   }
+
+  commitID = commitID.slice(0, 7);
+  versionAndCommitButton.addEventListener('click', function() {
+    if (versionAndCommitButton.textContent === version) {
+      versionAndCommitButton.textContent = commitID;
+    } else {
+      versionAndCommitButton.textContent = version;
+    }
+  });
 });
