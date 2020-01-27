d39f9d7c64103feff1750c152302d8eb007010c0
Dylan A. Orshefsky <daorshefsky@pbs.org>
2020-01-27 16:50:29 -0500

    Fix group slug change logic and validator

--- app/routes/groups/search.ts
+++ app/routes/groups/search.ts
@@ -2,16 +2,24 @@ import { Router } from 'express';
 import { getRepository, Like } from 'typeorm';
 import { Group } from '../../db';
 
-module.exports = Router().post('/', (req, res) =>
-  req.body.slug
-    ? getRepository(Group)
-        .find({ slug: Like(req.body.slug) })
-        .then(groups => res.send(groups))
-    : getRepository(Group)
+module.exports = Router().post('/', (req, res) => {
+  if(req.body.slug){
+    getRepository(Group)
+        .find({ slug: req.body.slug })
+        .then(groups => {
+          if(groups.length !== 0){
+            res.send(groups);
+          } else {
+            res.send(false);
+          }
+        })
+  } else {
+    getRepository(Group)
         .createQueryBuilder('group')
         .where('LOWER(group.name) LIKE :search', {
           search: `%${req.body.search.toLowerCase()}%`
         })
         .getMany()
         .then(groups => res.send(groups))
-);
+  }
+});
